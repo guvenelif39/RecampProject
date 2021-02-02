fa4705d7aa60f085b29772740a8abeccb36dcfc2
guvenelif39 <76227099+guvenelif39@users.noreply.github.com>
2021-02-02 23:43:25 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    class InMemoryCarDal : ICarDal
+
+    {
+        List<Car> _car;
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2011,DailyPrice=100000,Description="36.000 km"},
+                new Car{Id=2,BrandId=1,ColorId=1,ModelYear=2017,DailyPrice=98000,Description="45.000 km"},
+                new Car{Id=3,BrandId=2,ColorId=2,ModelYear=2020,DailyPrice=190000,Description="0 km"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _car.SingleOrDefault(p => p.Id == car.Id);
+            _car.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _car.Where(p => p.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = null;
+            carToUpdate = _car.SingleOrDefault(p => p.Id == car.Id);
+            car.Id = carToUpdate.Id;
+            car.BrandId = carToUpdate.BrandId;
+            car.ColorId = carToUpdate.ColorId;
+            car.ModelYear = carToUpdate.ModelYear;
+            car.DailyPrice = carToUpdate.DailyPrice;
+            car.Description = carToUpdate.Description;
+
+        }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    // Çıplak kalmamalı
+   public class Car:IEntity
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        public Double DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
